9b94d2ab2e758cbc5edf2041e25cba9517126d72
abubakarsaddique22 <abubakarsadddique3434@gmail.com>
2025-04-20 12:51:23 +0500

    minor change

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-def add(a,b):
-    return a+b 
+def add(a, b):
+    return a + b 
 
 
+def sub(a, b):
+    return a - b
 
+ 
+result1=add(4, 5)
+result2=sub(4, 5)
 
-
-def sub(a,b):return a-b 
-
-result1=add(4,5)
-result2=sub(4,5)
-
-# ssh-keygen -t ed25519 -C "abubakarsaddique3434@gmail.com"
+print(result1, result2)
 
 
